14db06a7c29f7b3c68a09cbe0f20d2da420e158e
Veld <velddev@gmail.com>
2018-08-10 21:42:15 +0200

    Add: Miki.Cache.InMemory Remove ISerializer and moved it to Miki.Serializer

---
+++ Miki.Cache.InMemory/InMemoryCacheClient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miki.Cache.InMemory
+{
+	public class InMemoryCacheClient : ICacheClient
+	{
+		private ConcurrentDictionary<string, byte[]> _dictionary;
+		private ISerializer _serializer;
+
+		public InMemoryCacheClient(ISerializer serializer)
+		{
+			_dictionary = new ConcurrentDictionary<string, byte[]>();
+			_serializer = serializer;
+		}
+		public InMemoryCacheClient(ConcurrentDictionary<string, byte[]> dictionary, ISerializer serializer)
+		{
+			_dictionary = dictionary;
+			_serializer = serializer;
+		}
+
+		public async Task<bool> ExistsAsync(string key)
+		{
+			await Task.Yield();
+			return _dictionary.ContainsKey(key);
+		}
+
+		public async Task<T> GetAsync<T>(string key)
+		{
+			if(await ExistsAsync(key))
+			{
+				return _serializer.Deserialize<T>(_dictionary[key]);
+			}
+			return default(T);
+		}
+
+		public async Task RemoveAsync(string key)
+		{
+			if(await ExistsAsync(key))
+			{
+				_dictionary.TryRemove(key, out var x);
+			}
+		}
+
+		public async Task UpsertAsync<T>(string key, T value, TimeSpan? expiresIn = null)
+		{
+			await Task.Yield();
+			_dictionary.AddOrUpdate(key, _serializer.Serialize(value), (x, y) => _serializer.Serialize(value));
+		}
+	}
+}
---
+++ Miki.Cache.InMemory/InMemoryCachePool.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Miki.Cache.InMemory
+{
+	public class InMemoryCachePool : ICachePool
+	{
+		private Dictionary<string, byte[]> _cache = new Dictionary<string, byte[]>();
+
+		public ICacheClient Get => throw new NotImplementedException();
+	}
+}
