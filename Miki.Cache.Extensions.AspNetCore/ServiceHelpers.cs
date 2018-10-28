aebe835c9a87a45753069320db8ade6df16a6b96
Veld <velddev@gmail.com>
2018-10-28 19:17:49 +0100

    Added generic solution for asp.netcore

---
+++ Miki.Cache.Extensions.AspNetCore/ServiceHelpers.cs
@@ -0,0 +1,19 @@
+﻿using Miki.Cache;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+	public static class ServiceHelpers
+	{
+		public static IServiceCollection AddCacheClient(this IServiceCollection collection, ICacheClient client)
+		{
+			return collection.AddSingleton(client);
+		}
+
+		public static IServiceCollection AddCacheClient(this IServiceCollection collection, ICachePool pool)
+		{
+			return collection.AddSingleton(pool)
+				.AddScoped((provider) => provider.GetService<ICachePool>().GetAsync().Result);
+		}
+	}
+}
